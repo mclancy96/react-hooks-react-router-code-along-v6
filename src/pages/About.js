56af9af5a0b8809633eff178ee14e5c6aec743e9
Mike <mclancy96@gmail.com>
2025-06-17 09:25:05 -0400

    Complete Code Along

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/Navbar";
+
+function About() {
+	return (
+		<>
+			<header>{<NavBar/>}</header>
+			<main>
+				<h1>This is my about component!</h1>
+			</main>
+		</>
+	);
+}
+
+export default About;
